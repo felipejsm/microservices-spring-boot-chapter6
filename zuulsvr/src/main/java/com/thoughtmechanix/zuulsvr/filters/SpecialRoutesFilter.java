74159f27551da309f899c96fcf7995e2e5fe510a
felipejsm <felipejsm41@gmail.com>
2020-07-29 07:43:11 -0300

    Special Filter

---
+++ zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/SpecialRoutesFilter.java
@@ -0,0 +1,35 @@
+package com.thoughtmechanix.zuulsvr.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SpecialRoutesFilter extends ZuulFilter {
+    private static final int FILTER_ORDER = 1;
+    private static final boolean SHOULD_FILTER = true;
+    private static final Logger logger = LoggerFactory.getLogger(SpecialRoutesFilter.class);
+
+    @Autowired
+    FilterUtils filterUtils;
+    @Override
+    public String filterType() {
+        return FilterUtils.ROUTE_FILTER_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FILTER_ORDER;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return SHOULD_FILTER;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
